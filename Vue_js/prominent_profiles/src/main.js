e081799e316771874b0befbf5af05271c4c3a0ac
Anthony Meek <ajm018@student.bham.ac.uk>
2023-12-30 19:09:45 +0000

    - FaGoogle icon import.

--- Vue_js/prominent_profiles/src/main.js
+++ Vue_js/prominent_profiles/src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faExternalLinkAlt, faCircleChevronUp, faCircleChevronDown, faCircleMinus, faShuffle, faMagnifyingGlassChart } from '@fortawesome/free-solid-svg-icons'
+import { faGoogle } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 library.add(faExternalLinkAlt)
@@ -12,6 +13,7 @@ library.add(faCircleChevronDown)
 library.add(faCircleMinus)
 library.add(faShuffle)
 library.add(faMagnifyingGlassChart)
+library.add(faGoogle)
 
 const app = createApp(App).use(router)
 
