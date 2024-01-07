1730057eeeba24957a3d11bcc5e307daf94ae164
Anthony Meek <ajm018@student.bham.ac.uk>
2024-01-07 23:52:26 +0000

    - Add PageFooter.vue to HomePage.vue

--- Vue_js/prominent_profiles/src/views/HomePage.vue
+++ Vue_js/prominent_profiles/src/views/HomePage.vue
@@ -3,9 +3,10 @@
   <div>
     <TrendingProfileList/>
   </div>
-
+   <PageFooter/>
 </template>
 
 <script setup>
 import TrendingProfileList from '../components/TrendingProfiles.vue'
+import PageFooter from '@/components/PageFooter.vue'
 </script>
