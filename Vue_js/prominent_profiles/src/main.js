7cc9443e4cc7895b9ccafe9bad7857b556e4903d
Anthony Meek <ajm018@student.bham.ac.uk>
2024-01-09 18:35:10 +0000

    main.js
    - Importing Calendar, Datepicker from v-calendar for use by SortToggle.vue
    - Adding appropriate font awesome icons for toggles on EntityPage.vue

--- Vue_js/prominent_profiles/src/main.js
+++ Vue_js/prominent_profiles/src/main.js
@@ -3,9 +3,11 @@ import App from './App.vue'
 import router from './router'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faExternalLinkAlt, faCircleChevronUp, faCircleChevronDown, faCircleMinus, faShuffle, faMagnifyingGlassChart } from '@fortawesome/free-solid-svg-icons'
+import { faExternalLinkAlt, faCircleChevronUp, faCircleChevronDown, faCircleMinus, faShuffle, faMagnifyingGlassChart, faPercent, faClock, faArrowUp, faArrowDown, faCalendarDays } from '@fortawesome/free-solid-svg-icons'
 import { faGoogle } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { setupCalendar, Calendar, DatePicker } from 'v-calendar'
+import 'v-calendar/style.css'
 
 library.add(faExternalLinkAlt)
 library.add(faCircleChevronUp)
@@ -13,9 +15,17 @@ library.add(faCircleChevronDown)
 library.add(faCircleMinus)
 library.add(faShuffle)
 library.add(faMagnifyingGlassChart)
+library.add(faPercent)
+library.add(faClock)
+library.add(faArrowUp)
+library.add(faArrowDown)
 library.add(faGoogle)
+library.add(faCalendarDays)
 
 const app = createApp(App).use(router)
 
 app.mount('#app')
+app.use(setupCalendar, {})
 app.component('font-awesome-icon', FontAwesomeIcon)
+app.component('VCalendar', Calendar)
+app.component('VDatePicker', DatePicker)
