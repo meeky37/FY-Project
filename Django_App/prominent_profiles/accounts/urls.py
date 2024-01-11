77caedebdbccb3283b71ffcdc535c2ef3761882e
Anthony Meek <ajm018@student.bham.ac.uk>
2024-01-11 19:41:24 +0000

    Accounts Django App Changes
    - login response within line char limit now (more explicit).
    - get user data method to test Vue JS cookie setup in axios.js.

--- Django_App/prominent_profiles/accounts/urls.py
+++ Django_App/prominent_profiles/accounts/urls.py
@@ -1,15 +1,21 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
+
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
     TokenVerifyView,
 )
+from .views import get_user_data
+
+
+
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('api/get_user_data/', get_user_data, name='get_user_data'),
 ]
     # Need to add forgot password, registration, etc.
--- Django_App/prominent_profiles/accounts/views.py
+++ Django_App/prominent_profiles/accounts/views.py
@@ -1,8 +1,11 @@
+from rest_framework.authentication import SessionAuthentication
+from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework_simplejwt.tokens import RefreshToken
-from rest_framework.decorators import api_view, permission_classes
+from rest_framework.decorators import api_view, permission_classes, authentication_classes
 from rest_framework.response import Response
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from django.contrib.auth import authenticate
+
 
 @api_view(['POST'])
 @permission_classes([AllowAny])
@@ -19,6 +22,22 @@ def custom_login(request):
 
     if user is not None:
         refresh = RefreshToken.for_user(user)
-        return Response({'success': True, 'access_token': str(refresh.access_token), 'refresh_token': str(refresh)})
+        access_token = str(refresh.access_token)
+        refresh_token = str(refresh)
+
+        return Response(
+            {'success': True, 'access_token': access_token, 'refresh_token': refresh_token})
     else:
         return Response({'success': False, 'error': 'Invalid credentials'})
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+@authentication_classes([SessionAuthentication, JWTAuthentication])
+def get_user_data(request):
+    user = request.user
+    user_data = {
+        'first_name': user.first_name,
+        'last_name': user.last_name,
+    }
+    return Response(user_data)
