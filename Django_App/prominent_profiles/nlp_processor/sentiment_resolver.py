27fbc40cecdaf7b897c3f30a3d818f08436dae87
Anthony Meek <ajm018@student.bham.ac.uk>
2024-01-19 11:06:23 +0000

    sentiment_resolver.py
    - remove some print statements
    - remove unneeded imports
    - add bounderror object creation in excpetion cases
    - exception raises up to halt a further process on the article for attention of dev later using the bounderror table

--- Django_App/prominent_profiles/nlp_processor/sentiment_resolver.py
+++ Django_App/prominent_profiles/nlp_processor/sentiment_resolver.py
@@ -2,8 +2,9 @@ from decimal import Decimal, ROUND_HALF_UP
 from .constants import EXPONENTIAL_K_VALUE
 from intervaltree import Interval, IntervalTree
 from NewsSentiment import TargetSentimentClassifier
+
+from .models import BoundError
 from .utils import DatabaseUtils
-from profiles_app.models import Entity, Article
 
 
 def scaling(avg_array_list, k=3, linear=False):
@@ -33,7 +34,6 @@ def average_array(probabilities):
     for prob_data in probabilities:
         if not prob_data:
             continue
-        # print("prob_data:", prob_data)
         class_prob = prob_data['class_prob']
         class_label = prob_data['class_label']
 
@@ -53,7 +53,6 @@ def average_array(probabilities):
 
 def round_array_to_1dp(arr):
     decimal_array = [Decimal(str(x)) for x in arr]
-    original_sum = sum(decimal_array)
     rounded_array = [x.quantize(Decimal('0.0'), rounding=ROUND_HALF_UP) for x in decimal_array]
     rounded_sum = sum(rounded_array)
     adjustment = Decimal('100') - rounded_sum
@@ -80,6 +79,8 @@ class SentimentAnalyser:
             mention_segment = article_text[mention_start:mention_end]
             right_segment = article_text[mention_end:sentence_end]
 
+            # Could add logging here to see the quality of sentence segmentation.
+
             sentiment = self.tsc.infer_from_text(left_segment, mention_segment, right_segment)
             # print(sentiment[0])
 
@@ -91,7 +92,7 @@ class SentimentAnalyser:
             print(f"MENTION: {mention_segment}")
             print(f"RIGHT: {right_segment}")
 
-            BoundErrors.objects.create(
+            BoundError.objects.create(
                 bound_start=mention_start,
                 bound_end=mention_end,
                 left_segment=left_segment,
@@ -185,19 +186,6 @@ class SentimentAnalyser:
                                 'result': result
                             })
 
-                            # if cluster_id not in results_cache:
-                            #     result = self.bounds_sentiment(
-                            #         mention_start, mention_end, sentence_start, sentence_end,
-                            #         article_text
-                            #     )
-                            #     results_cache.setdefault(cluster_id, []).append(result)
-                            #     bounds_sentiment[bounds_key][entity_name][entity_db_id].append(
-                            #         result)
-                            # else:
-                            #     # print("Results Cache")
-                            #     # print(results_cache)
-                            #     results_cache[cluster_id].append(result)
-
                             if debug:
                                 print(
                                     START_HIGHLIGHT + f"{entity_name} - Mention ({mention_start}, {mention_end}) is within bounds ({sentence_start}, {sentence_end})")
@@ -254,20 +242,12 @@ class SentimentAnalyser:
                 # print("Bounds Keys:", bounds_keys[i])
                 # print()
 
-                article_instance, created = Article.objects.get_or_create(id=source_article_id)
-
-                # Example: Get or create an Entity instance
-                entity_instance, created = Entity.objects.get_or_create(id=entity_db_id)
-
                 DatabaseUtils.insert_bound_mention_data(entity_name, source_article_id,
                                                         entity_db_id,
                                                         scores, text[i],
                                                         bounds_keys[i])
 
-            # print('BOUND NUMBER')
             num_bound = len(averages['sentiment_scores'])
-            # print(num_bound)
-
             scaled_classification = scaling(averages['sentiment_scores'],
                                             k=EXPONENTIAL_K_VALUE)
 
@@ -282,25 +262,6 @@ class SentimentAnalyser:
                                                    linear=True)
             linear_percent = percentage_contribution(
                 linear_scaled_classification)
-
-            '''Creating a list of tuples, 'systems,' containing names and classifications of two 
-            points allocation approaches. Iterating over the list to print array values rounded 
-            to 1 decimal place, percentage contributions, and the majority class for each system.'''
-
-            systems = [("Exponential", scaled_classification),
-                       ("Linear", linear_scaled_classification)]
-
-            for system_name, system_classification in systems:
-                # print(f"Under {system_name} points system:")
-                # print(round_array_to_1dp(system_classification))
-                # print(percentage_contribution(system_classification))
-
-                majority_class = max(
-                    zip(["Neutral", "Positive", "Negative"], system_classification),
-                    key=lambda x: x[1])
-                # print(f"{system_name} {majority_class[0]} Majority")
-
-            print("------------------------------------------------")
 
             DatabaseUtils.insert_overall_sentiment(source_article_id, entity_db_id, num_bound,
                                                    linear_percent[0],
