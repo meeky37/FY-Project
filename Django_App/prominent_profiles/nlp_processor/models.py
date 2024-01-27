71eb72f18b0ceeb9104a8f806e470433203b096b
Anthony Meek <ajm018@student.bham.ac.uk>
2024-01-27 20:42:29 +0000

    Complete research in Article_Statistics_Playground.ipynb Evaluated the best fields and typical values. Create ArticleStatistics and SimilarArticlePair to store calculated statistics and matches that indicate a duplicate.

--- Django_App/prominent_profiles/nlp_processor/models.py
+++ Django_App/prominent_profiles/nlp_processor/models.py
@@ -1,7 +1,64 @@
 import os
-
 from django.db import models
 from django.conf import settings
+from profiles_app.models import Article
+
+class ArticleStatistics(models.Model):
+    article = models.OneToOneField(Article, on_delete=models.CASCADE, primary_key=True)
+
+    fuzzy_hash = models.CharField(max_length=64, null=True)
+    word_count = models.IntegerField(null=True, blank=True)
+    terms_count = models.IntegerField(null=True, blank=True)
+    vocd = models.FloatField(null=True, blank=True)
+    yulek = models.FloatField(null=True, blank=True)
+    simpsond = models.FloatField(null=True, blank=True)
+    the_count = models.IntegerField(null=True, blank=True)
+    and_count = models.IntegerField(null=True, blank=True)
+    is_count = models.IntegerField(null=True, blank=True)
+    of_count = models.IntegerField(null=True, blank=True)
+    in_count = models.IntegerField(null=True, blank=True)
+    to_count = models.IntegerField(null=True, blank=True)
+    it_count = models.IntegerField(null=True, blank=True)
+    that_count = models.IntegerField(null=True, blank=True)
+    with_count = models.IntegerField(null=True, blank=True)
+
+
+class SimilarArticlePair(models.Model):
+    article1 = models.ForeignKey(ArticleStatistics, related_name='article1',
+                                 on_delete=models.CASCADE)
+    article2 = models.ForeignKey(ArticleStatistics, related_name='article2',
+                                 on_delete=models.CASCADE)
+    hash_similarity_score = models.IntegerField()
+    words_diff = models.DecimalField(max_digits=5, decimal_places=2, null=True)
+    terms_diff = models.DecimalField(max_digits=5, decimal_places=2, null=True)
+    vocd_diff = models.DecimalField(max_digits=5, decimal_places=2, null=True)
+    yulek_diff = models.DecimalField(max_digits=5, decimal_places=2, null=True)
+    simpsond_diff = models.DecimalField(max_digits=5, decimal_places=2, null=True)
+    avg_count_diff = models.DecimalField(max_digits=5, decimal_places=2, null=True)
+    the_diff = models.DecimalField(max_digits=5, decimal_places=2, null=True)
+    and_diff = models.DecimalField(max_digits=5, decimal_places=2, null=True)
+    is_diff = models.DecimalField(max_digits=5, decimal_places=2, null=True)
+    of_diff = models.DecimalField(max_digits=5, decimal_places=2, null=True)
+    in_diff = models.DecimalField(max_digits=5, decimal_places=2, null=True)
+    to_diff = models.DecimalField(max_digits=5, decimal_places=2, null=True)
+    it_diff = models.DecimalField(max_digits=5, decimal_places=2, null=True)
+    that_diff = models.DecimalField(max_digits=5, decimal_places=2, null=True)
+    with_diff = models.DecimalField(max_digits=5, decimal_places=2, null=True)
+
+    def calculate_average_diff(self):
+        diff_fields = ['the_diff', 'and_diff', 'is_diff', 'of_diff', 'in_diff', 'to_diff',
+                       'it_diff', 'that_diff', 'with_diff']
+
+        diffs = [getattr(self, field) for field in diff_fields if getattr(self, field) is not None]
+
+        # Likely to be very few mentions or an
+        # extra message on page creating a huge % difference so filter.
+        diffs = [diff for diff in diffs if diff < 50]
+
+        if diffs:
+            return sum(diffs) / len(diffs)
+        else:
+            return None
 
 
 class ProcessedFile(models.Model):
@@ -12,6 +69,7 @@ class ProcessedFile(models.Model):
 
     def full_path(self):
         return os.path.join(settings.ARTICLE_SCRAPER_MEDIA_ROOT, self.media_path, self.file_name)
+
 
 class BoundError(models.Model):
     bound_start = models.IntegerField()
