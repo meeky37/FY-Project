0ed1cc1d8eae322e0ee25b44c6dae2dd130e469f
Anthony Meek <ajm018@student.bham.ac.uk>
2024-01-27 21:21:29 +0000

    views.py in profiles app
    Updating exp and linear overall sentiments to exclude all article2 id in a similar article pair given the provided articles for a particular entity id. Hence, only article 1 will show in frontend.
    
    Next Steps: Think about how I can integrate duplicate detection into scraping a job to prevent wasteful analysis. It May require inserting into the article database earlier on.

--- Django_App/prominent_profiles/profiles_app/views.py
+++ Django_App/prominent_profiles/profiles_app/views.py
@@ -1,15 +1,15 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import JsonResponse
-from django.utils import timezone
+from datetime import timedelta
+from django.shortcuts import get_object_or_404
 from django.views import View
-from .models import Entity, BingEntity, OverallSentiment, Article, EntityView
+from .models import Entity, BingEntity, OverallSentiment
+from nlp_processor.models import SimilarArticlePair
 
 
 # TODO: Remove unneeded attributes from JSON views.
 
 class VisibleEntitiesView(View):
     def get(self, request, *args, **kwargs):
-        visible_entities = Entity.objects.filter(app_visible=True)
+        visible_entities = Entity.objects.filter(app_visible=True).order_by('name')
         serialized_entities = [
             {'id': entity.id, 'name': entity.name}
             for entity in visible_entities
@@ -34,11 +34,6 @@ class BingEntityDetailView(View):
             'entity_type_hints': bing_entity.entity_type_hints,
             'date_added': bing_entity.date_added
         }
-        # serialized_entity = {
-        #     'id': bing_entity.id,
-        #     'description': bing_entity.description,
-        #     'image_url': bing_entity.image_url,
-        # }
         return JsonResponse(serialized_entity, safe=False)
 
 
@@ -54,51 +49,52 @@ class BingEntityMiniView(View):
             'contractual_rules': bing_entity.contractual_rules,
             'display_hint': bing_entity.entity_type_display_hint
         }
-        # serialized_entity = {
-        #     'id': bing_entity.id,
-        #     'description': bing_entity.description,
-        #     'image_url': bing_entity.image_url,
-        # }
         return JsonResponse(serialized_entity, safe=False)
-
-
-from django.shortcuts import get_object_or_404
-from .models import OverallSentiment, Article
 
 
 class OverallSentimentExp(View):
     def get(self, request, *args, **kwargs):
         entity_id = kwargs.get('entity_id')
 
-        # Retrieve a queryset of OverallSentiment objects for the given entity_id
         overall_sentiments = OverallSentiment.objects.filter(entity=entity_id)
-
-        # Handle the case where no object is found
         if not overall_sentiments.exists():
             return JsonResponse({'error': 'No OverallSentiment found for the given entity_id'},
                                 status=404)
 
-        # If one or more objects are found, serialize each object
+        has_similar_pair = SimilarArticlePair.objects.filter(
+            Q(article2_id__in=[sentiment.article.id for sentiment in overall_sentiments]),
+            Q(hash_similarity_score__gte=90) |
+            (
+                    Q(hash_similarity_score__gte=65, words_diff__lt=10, terms_diff__lt=10,
+                      vocd_diff__lt=5, yulek_diff__lt=10, simpsond_diff__lt=10,
+                      the_diff__lt=20, and_diff__lt=20, is_diff__lt=20,
+                      of_diff__lt=20, in_diff__lt=20, to_diff__lt=20,
+                      it_diff__lt=20, that_diff__lt=20, with_diff__lt=20
+                      ) & Q(
+                article2__article__publication_date__lte=F(
+                    'article1__article__publication_date') + timedelta(days=14)
+            )
+            )
+        ).values_list('article2_id', flat=True)
+
+        print(has_similar_pair)
+
         serialized_entities = []
         for overall_sentiment in overall_sentiments:
-            # Assuming 'article' is the ForeignKey field in OverallSentiment
-            article = get_object_or_404(Article, id=overall_sentiment.article.id)
-
-            serialized_entity = {
-                'id': overall_sentiment.article.id,
-                'entity_id': overall_sentiment.entity.id,
-                'headline': overall_sentiment.article.headline,
-                'url': overall_sentiment.article.url,
-                'image_url': overall_sentiment.article.image_url,
-                'publication_date': overall_sentiment.article.publication_date,
-                'author': overall_sentiment.article.author,
-                'neutral': overall_sentiment.exp_neutral,
-                'positive': overall_sentiment.exp_positive,
-                'negative': overall_sentiment.exp_negative
-            }
-
-            serialized_entities.append(serialized_entity)
-
+            if overall_sentiment.article.id not in has_similar_pair:
+                serialized_entity = {
+                    'id': overall_sentiment.article.id,
+                    'entity_id': overall_sentiment.entity.id,
+                    'headline': overall_sentiment.article.headline,
+                    'url': overall_sentiment.article.url,
+                    'image_url': overall_sentiment.article.image_url,
+                    'publication_date': overall_sentiment.article.publication_date,
+                    'author': overall_sentiment.article.author,
+                    'neutral': overall_sentiment.exp_neutral,
+                    'positive': overall_sentiment.exp_positive,
+                    'negative': overall_sentiment.exp_negative
+                }
+                serialized_entities.append(serialized_entity)
         return JsonResponse(serialized_entities, safe=False)
 
 
@@ -106,22 +102,31 @@ class OverallSentimentLinear(View):
 
     def get(self, request, *args, **kwargs):
         entity_id = kwargs.get('entity_id')
-
-        # Retrieve a queryset of OverallSentiment objects for the given entity_id
         overall_sentiments = OverallSentiment.objects.filter(entity=entity_id)
-
-        # Handle the case where no object is found
         if not overall_sentiments.exists():
             return JsonResponse({'error': 'No OverallSentiment found for the given entity_id'},
                                 status=404)
 
-        # If one or more objects are found, serialize each object
+        has_similar_pair = SimilarArticlePair.objects.filter(
+            Q(article2_id__in=[sentiment.article.id for sentiment in overall_sentiments]),
+            Q(hash_similarity_score__gte=90) |
+            (
+                    Q(hash_similarity_score__gte=65, words_diff__lt=10, terms_diff__lt=10,
+                      vocd_diff__lt=5, yulek_diff__lt=10, simpsond_diff__lt=10,
+                      the_diff__lt=20, and_diff__lt=20, is_diff__lt=20,
+                      of_diff__lt=20, in_diff__lt=20, to_diff__lt=20,
+                      it_diff__lt=20, that_diff__lt=20, with_diff__lt=20
+                      ) & Q(
+                article2__article__publication_date__lte=F(
+                    'article1__article__publication_date') + timedelta(days=14)
+            )
+            )
+        ).values_list('article2_id', flat=True)
+
         serialized_entities = []
         for overall_sentiment in overall_sentiments:
-            # Assuming 'article' is the ForeignKey field in OverallSentiment
-            article = get_object_or_404(Article, id=overall_sentiment.article.id)
-
-            serialized_entity = {
+            if overall_sentiment.article.id not in has_similar_pair:
+                serialized_entity = {
                 'id': overall_sentiment.article.id,
                 'entity_id': overall_sentiment.entity.id,
                 'headline': overall_sentiment.article.headline,
@@ -129,9 +134,9 @@ class OverallSentimentLinear(View):
                 'image_url': overall_sentiment.article.image_url,
                 'publication_date': overall_sentiment.article.publication_date,
                 'author': overall_sentiment.article.author,
-                'neutral': overall_sentiment.exp_neutral,
-                'positive': overall_sentiment.exp_positive,
-                'negative': overall_sentiment.exp_negative
+                'neutral': overall_sentiment.linear_neutral,
+                'positive': overall_sentiment.linear_positive,
+                'negative': overall_sentiment.linear_negative
             }
 
             serialized_entities.append(serialized_entity)
@@ -152,7 +157,7 @@ class ArticleOverallSentimentExp(View):
             return JsonResponse({'error': 'No OverallSentiment found for the given article_id'},
                                 status=404)
 
-        # Sort the queryset to prioritise entry id provided for article detail for that
+        # Sort the queryset to prioritize the entry id provided for article detail for that
         # click-thru.
         overall_sentiments = sorted(
             overall_sentiments,
@@ -212,7 +217,7 @@ def create_entity_view(request, entity_id):
 
 from django.utils import timezone
 from django.http import JsonResponse
-from django.db.models import Sum, Count
+from django.db.models import Sum, Count, Q, F
 from .models import EntityView
 
 
@@ -240,7 +245,5 @@ def get_trending_entities(request):
 
     data = [{'entity_id': entity['entity__id'], 'entity_name': entity['entity__name'],
              'total_views': entity['total_views']} for entity in top_entities[:6]]
-
-
 
     return JsonResponse({'trending_entities': data})
