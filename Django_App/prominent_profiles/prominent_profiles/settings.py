da92ceaa06af14743b16a2bf69e4096381a599e3
Anthony Meek <ajm018@student.bham.ac.uk>
2024-02-12 21:39:26 +0000

    Update port to digital ocean stipulation

--- Django_App/prominent_profiles/prominent_profiles/settings.py
+++ Django_App/prominent_profiles/prominent_profiles/settings.py
@@ -27,7 +27,7 @@ else:
     SECRET_KEY = 'django-insecure-15i*(zucahz)+@8ikq9!%4dibe3#bjubrl4xmh17yjcbmabt9i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["localhost"]
 
@@ -166,7 +166,7 @@ if os.getenv('RUNNING_IN_DOCKER', 'False') == 'True':
             'USER': os.getenv('DB_USER', 'django_access'),
             'PASSWORD': os.getenv('DB_PASSWORD', ''),
             'HOST': os.getenv('DB_HOST', 'db'),
-            'PORT': os.getenv('DB_PORT', '5432'),
+            'PORT': os.getenv('DB_PORT', '25060'),
         }
     }
 else:
