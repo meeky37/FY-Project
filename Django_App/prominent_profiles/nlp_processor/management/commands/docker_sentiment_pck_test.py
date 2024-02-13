8e59369a8e7b448ad9f9144e4c926c9dbce167e6
Anthony Meek <ajm018@student.bham.ac.uk>
2024-02-13 16:27:35 +0000

    - MPS device (macbook M1) unlikely to work on docker container on digital ocean droplet - changing to CPU for now.

--- Django_App/prominent_profiles/nlp_processor/management/commands/docker_sentiment_pck_test.py
+++ Django_App/prominent_profiles/nlp_processor/management/commands/docker_sentiment_pck_test.py
@@ -2,14 +2,11 @@ from NewsSentiment import TargetSentimentClassifier
 from django.core.management import BaseCommand
 from fastcoref import FCoref
 
-from Django_App.prominent_profiles.nlp_processor.constants import F_COREF_DEVICE
-
-
 class Command(BaseCommand):
     help = 'Test News MTSC and FastCoref works on docker setup (as some MAC mps configs!)'
 
     def handle(self, *args, **options):
-        model = FCoref(device=F_COREF_DEVICE)
+        model = FCoref(device='cpu')
 
         preds = model.predict(
             texts=['We are so happy to see you using our coref package. This package is very fast!']
