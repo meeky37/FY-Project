2e7a7321e230cb2509a61793edaf9f649ad2f5bf
Anthony Meek <ajm018@student.bham.ac.uk>
2024-02-17 19:44:06 +0000

    - /django api path in nginx.conf and vue js

--- Vue_js/prominent_profiles/src/config.js
+++ Vue_js/prominent_profiles/src/config.js
@@ -1,2 +1,2 @@
-export const API_BASE_URL = 'https://www.prominentprofiles.com'
+export const API_BASE_URL = 'https://www.prominentprofiles.com/django'
 // export const API_BASE_URL = 'http://localhost:8000'
