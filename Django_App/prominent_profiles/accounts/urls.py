5c99d4578b64fc71b0fefb80bde153851c21be9d
Anthony Meek <ajm018@student.bham.ac.uk>
2024-02-22 12:09:46 +0000

    Password Reset done uncommented - Following a debug suggestion from django

--- Django_App/prominent_profiles/accounts/urls.py
+++ Django_App/prominent_profiles/accounts/urls.py
@@ -24,8 +24,8 @@ urlpatterns = [
     path('api/password_reset/', PasswordResetView.as_view(), name='password_reset_request'),
     path('api/password_reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(),
          name='password_reset_confirm'),
-    # path('api/password_reset/done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
-    # path('reset/done/', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('api/password_reset/done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/done/', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
 
 # TODO: Need to add forgot password still! Done?
