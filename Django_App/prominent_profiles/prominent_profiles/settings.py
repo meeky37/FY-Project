eba39bff915cd7a26e7363b0d8f8044efae16e96
Anthony Meek <ajm018@student.bham.ac.uk>
2024-02-22 00:35:40 +0000

    Auth Error -> Trying different ZOHO host

--- Django_App/prominent_profiles/prominent_profiles/settings.py
+++ Django_App/prominent_profiles/prominent_profiles/settings.py
@@ -262,7 +262,7 @@ ADMIN_SHORTCUTS = [
 ]
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.zoho.com'
+EMAIL_HOST = 'smtppro.zoho.eu'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'info@prominentprofiles.com'
