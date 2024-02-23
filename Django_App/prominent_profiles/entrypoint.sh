9f2c5fa00b684cc09c692d6ffb43f6ea7e527885
Anthony Meek <ajm018@student.bham.ac.uk>
2024-02-23 19:46:36 +0000

    u+w permissions
    Try using web image to build celery/celery beat.
    Import optimisation admin.py, article_processor.py, bing_api.py

--- Django_App/prominent_profiles/entrypoint.sh
+++ Django_App/prominent_profiles/entrypoint.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 # entrypoint.sh
 
-# Ownership of the mounted volumes for django-user
+# Change ownership of the mounted volumes to django-user
 chown -R django-user:django /app/nlp_processor/media
 chown -R django-user:django /app/static
-
 chown -R django-user:django /app
+
+# Write permission for the django-user (to save/delete etc)
+chmod -R u+w /app/nlp_processor/media
+chmod -R u+w /app/static
+chmod -R u+w /app
 
 # Switching back to django-user
 exec gosu django-user "$@"
