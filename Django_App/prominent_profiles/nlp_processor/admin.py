fbd1087dcf6eb422b0c77eca57a151a988536894
Anthony Meek <ajm018@student.bham.ac.uk>
2024-02-29 09:20:38 +0000

    NLP Applied True Admin Function

--- Django_App/prominent_profiles/nlp_processor/admin.py
+++ Django_App/prominent_profiles/nlp_processor/admin.py
@@ -4,8 +4,9 @@ from django.contrib import admin
 from django.db.models import F
 from django.db.models import Case, When, Value
 from django.db import models
-from .models import ProcessedFile, BoundError, ArticleStatistics, SimilarArticlePair
 from django.utils.translation import gettext as _
+
+from .models import ProcessedFile, BoundError, ArticleStatistics, SimilarArticlePair
 
 
 class ProcessedFileAdmin(admin.ModelAdmin):
@@ -14,7 +15,14 @@ class ProcessedFileAdmin(admin.ModelAdmin):
     def set_nlp_applied_false(self, request, queryset):
         queryset.update(nlp_applied=False)
 
-    set_nlp_applied_false.short_description = "Set NLP Applied to False for selected items"
+    set_nlp_applied_false.short_description = "Set NLP Applied to FALSE for selected items"
+
+    actions = [set_nlp_applied_false]
+
+    def set_nlp_applied_true(self, request, queryset):
+        queryset.update(nlp_applied=True)
+
+    set_nlp_applied_true.short_description = "Set NLP Applied to TRUE for selected items"
 
     actions = [set_nlp_applied_false]
 
@@ -30,7 +38,6 @@ class DuplicatePrediction(admin.SimpleListFilter):
         )
 
     def queryset(self, request, queryset):
-        # TODO: QUESTION MARK OVER wether this filtering works!
         if self.value() == 'high':
             return queryset.filter(hash_similarity_score__gte=90) | (
                 queryset.filter(
@@ -68,15 +75,17 @@ class SimilarArticlePairAdmin(admin.ModelAdmin):
     list_filter = [DuplicatePrediction]
     search_fields = ['article1__article__id', 'article2__article__id']
     readonly_fields = ['get_article_1_headline', 'get_article_2_headline'
-                       , 'get_article_1_site_name', 'get_article_2_site_name']
+        , 'get_article_1_site_name', 'get_article_2_site_name']
 
     def get_article_1_headline(self, obj):
         return obj.article1.article.headline
+
     get_article_1_headline.short_description = 'Article 1 Headline'
     get_article_1_headline.admin_order_field = 'article1__headline'
 
     def get_article_2_site_name(self, obj):
         return obj.article2.article.site_name
+
     get_article_2_site_name.short_description = 'Article 2 Site Name'
     get_article_2_site_name.admin_order_field = 'article2__site_name'
 
@@ -92,13 +101,17 @@ class SimilarArticlePairAdmin(admin.ModelAdmin):
     get_article_2_headline.short_description = 'Article 2 Headline'
     get_article_2_headline.admin_order_field = 'article2__headline'
 
+
 class BoundErrorAdmin(admin.ModelAdmin):
     list_display = ('article', 'bound_start', 'bound_end', 'left_segment', 'mention_segment',
                     'right_segment', 'error_message', 'timestamp')
 
 
 class ArticleStatisticsAdmin(admin.ModelAdmin):
-    list_display = ('article', 'fuzzy_hash', 'word_count', 'terms_count', 'vocd', 'yulek', 'simpsond', 'the_count', 'and_count', 'is_count', 'of_count', 'in_count', 'to_count', 'it_count', 'that_count', 'with_count')
+    list_display = (
+    'article', 'fuzzy_hash', 'word_count', 'terms_count', 'vocd', 'yulek', 'simpsond', 'the_count',
+    'and_count', 'is_count', 'of_count', 'in_count', 'to_count', 'it_count', 'that_count',
+    'with_count')
 
 
 admin.site.register(ProcessedFile, ProcessedFileAdmin)
