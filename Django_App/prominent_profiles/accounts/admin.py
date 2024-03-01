dad9c7d2498723b54213bf602c906d988ee11028
Anthony Meek <ajm018@student.bham.ac.uk>
2024-03-01 18:54:06 +0000

    Accounts App Comments

--- Django_App/prominent_profiles/accounts/admin.py
+++ Django_App/prominent_profiles/accounts/admin.py
@@ -4,24 +4,36 @@ from .models import CustomUser, Subscription
 
 
 class UserAdmin(BaseUserAdmin):
-    list_display = ('email', 'phone_number', 'date_of_birth', 'email_verified', 'location', 'first_name', 'last_name', 'is_active', 'is_staff')
-    search_fields = ('email', 'phone_number', 'date_of_birth', 'location', 'first_name', 'last_name')
+    """
+    Provides an appropriate list view of entities.
+    Enables search based on many factors for easy user look up.
+    """
+
+    list_display = (
+    'email', 'phone_number', 'date_of_birth', 'email_verified', 'location', 'first_name',
+    'last_name', 'is_active', 'is_staff')
+    search_fields = (
+    'email', 'phone_number', 'date_of_birth', 'location', 'first_name', 'last_name')
     ordering = ('email',)
 
     fieldsets = (
         (None, {'fields': ('email', 'password', 'last_visit', 'last_visit_excluding_today')}),
-        ('Personal info', {'fields': ('phone_number', 'date_of_birth', 'email_verified', 'location', 'first_name', 'last_name')}),
-        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Personal info', {'fields': (
+        'phone_number', 'date_of_birth', 'email_verified', 'location', 'first_name', 'last_name')}),
+        ('Permissions',
+         {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
         # ('Important dates', {'fields': ('last_login', 'date_joined')}),
     )
 
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2', 'phone_number', 'date_of_birth', 'email_verified', 'location', 'first_name', 'last_name'),
+            'fields': (
+            'email', 'password1', 'password2', 'phone_number', 'date_of_birth', 'email_verified',
+            'location', 'first_name', 'last_name'),
         }),
     )
 
-admin.site.register(CustomUser, UserAdmin)
 
+admin.site.register(CustomUser, UserAdmin)
 admin.site.register(Subscription)
--- Django_App/prominent_profiles/accounts/middleware.py
+++ Django_App/prominent_profiles/accounts/middleware.py
@@ -3,6 +3,15 @@ from django.utils.deprecation import MiddlewareMixin
 
 
 class UserActivityMiddleware(MiddlewareMixin):
+    """
+    Middleware to track user activity by updating 'last_visit' and 'last_visit_excluding_today'
+    fields on the user model.
+
+    For every request where the user is logged in, this middleware updates the 'last_visit' field to
+    the current timestamp. Next, it updates 'last_visit_excluding_today' with the timestamp of the
+    user's last visit that was not today; this supports the articles added since last visit counters
+    system in the Prominent Profiles Dashboard.
+    """
     def process_request(self, request):
         if request.user.is_authenticated:
             now = timezone.now()
