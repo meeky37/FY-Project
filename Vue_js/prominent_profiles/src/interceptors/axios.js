a8b0ec6d7d5446e8f9c78bc2617560b2aaf4c0ac
Anthony Meek <ajm018@student.bham.ac.uk>
2024-03-05 23:34:29 +0000

    - Simplifying axios.js by removing max attempts counter

--- Vue_js/prominent_profiles/src/interceptors/axios.js
+++ Vue_js/prominent_profiles/src/interceptors/axios.js
@@ -20,10 +20,6 @@ axios.interceptors.request.use(
     return Promise.reject(error)
   }
 )
-
-let refreshAttempts = 0
-const MAX_REFRESH_ATTEMPTS = 3 // max number of refresh attempts to prevent hogging connection
-
 axios.interceptors.response.use(
   (response) => {
     return response
@@ -31,36 +27,27 @@ axios.interceptors.response.use(
   async (error) => {
     const originalRequest = error.config
 
-    // Check if the error is due to an expired access token
+    // Check if the error is due to an expired access token and we haven't retried yet
     if (error.response.status === 401 && !originalRequest._retry) {
-      originalRequest._retry = true
+      originalRequest._retry = tru
 
-      // Attempt to refresh the access token using the refresh token
-      if (refreshAttempts < MAX_REFRESH_ATTEMPTS) {
-        const refreshToken = VueCookies.get('refresh_token')
-        if (refreshToken) {
-          try {
-            const response = await axios.post(
-              `${API_BASE_URL}/accounts/api/token/refresh/`, {
-                refresh: refreshToken
-              })
+      const refreshToken = VueCookies.get('refresh_token')
+      if (refreshToken) {
+        try {
+          const response = await axios.post(`${API_BASE_URL}/accounts/api/token/refresh/`, {
+            refresh: refreshToken
+          })
 
-            VueCookies.set('access_token', response.data.access)
-            refreshAttempts = 0 // reset the counter on successful refresh
-            return axios(originalRequest)
-          } catch (refreshError) {
-            refreshAttempts += 1 // incrementing counter
-            console.error('Token refresh failed', refreshError)
-            // If max attempts reached, logout the user
-            if (refreshAttempts >= MAX_REFRESH_ATTEMPTS) {
-              VueCookies.delete('access_token')
-              VueCookies.delete('refresh_token')
-              await router.push('/login')
-            }
-          }
+          VueCookies.set('access_token', response.data.access)
+          return axios(originalRequest) // Retry the original request with the new token
+        } catch (refreshError) {
+          // If it fails we delete tokens and redirect to login page
+          VueCookies.delete('access_token')
+          VueCookies.delete('refresh_token')
+          await router.push('/login')
         }
       } else {
-        // Directly logout the user if max attempts have already been reached without trying to refresh
+        // No refresh token available, let's go to login page
         VueCookies.delete('access_token')
         VueCookies.delete('refresh_token')
         await router.push('/login')
